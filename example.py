4159062d5699292fe2b8c881385dc1b7b1215f58
samlaihei <82487191+samlaihei@users.noreply.github.com>
2023-05-02 11:17:17 +1000

    Update example

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 # Continuum Fitting Windows #
 conti_windows = np.array([[1973., 1983.], [2060., 2340.], [2600., 2740.], [2840., 3100.]])
-	
+
 # Filepath with Lines Definition #
 line_path = 'Lines/Lines_MgII.csv'
 
@@ -23,7 +23,7 @@ line_fit_MgII = [[2750., 2850.]]
 example = PyQSpecFit.PyQSpecFit()
 
 # Perform fits #
-example.runFit(line_path, conti_windows, line_fit_MgII, N_fits = 10, syntheticFits=True, useFe=True)
+example.runFit(line_path, conti_windows, line_fit_MgII, N_fits = 1, useFe=True)
 
 # Evaluate line #
 example.evalLineProperties(line_path, 'Line_Params/example.csv', 0.83, lineCompInd=0)
@@ -33,7 +33,6 @@ fig, axs = plt.subplots(2,1, figsize=(8, 6), gridspec_kw=dict(height_ratios=[3,1
 plt.subplots_adjust(wspace= 0.30, hspace= 0.00)
 example.plotLineFits(axs[0], axs[1], line_path, 'data/example.csv', 'Line_Params/example.csv', 0.83, plotWindow=[2500, 3100])
 plt.savefig('Fit_Figs/example.png', dpi=200, bbox_inches='tight')
-
 
 
 

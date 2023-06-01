094c47abe1bf654175fbf187081912330f466731
samlaihei <82487191+samlaihei@users.noreply.github.com>
2023-06-01 18:39:15 +1000

    Update example.py

--- example.py
+++ example.py
@@ -1,10 +1,7 @@
 # Example of running PyQSpecFit from special parameter file
 import PyQSpecFit
-import pandas as pd
-import matplotlib.pyplot as plt
 
 file = 'Run_Files/runFile.csv'
-pdata = pd.read_csv(file)
 
 example = PyQSpecFit.PyQSpecFit()
 
